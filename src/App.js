648794d4d4a74fc297ea6f47fef2121ec517f2fc
陶松 <talents@talents.lan>
2018-10-06 21:43:17 +0800

    1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import { DatePicker } from 'antd';
+//import 'antd/lib/date-picker/style/css'; 
 class App extends Component {
   render() {
     return (
       <div className="App">
+       <DatePicker />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
